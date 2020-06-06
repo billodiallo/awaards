1ea1408528f617b264fd990dcbaf9305d00e4c6a
Mamadou Billo Diallo <billodiallonet@gmail.com>
2020-06-06 09:41:37 +0300

    dual mode

--- awaards/settings.py
+++ awaards/settings.py
@@ -29,9 +29,35 @@ STATICFILES_DIRS = (
 SECRET_KEY = 'kfp)qt#c34s)%0zsus&nw(h7v4@$l#_-ix+*41h=3k0zf43e6k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+MODE=config("MODE", default="dev")
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+# development
+if config('MODE')=="dev":
+   DATABASES = {
+       'default': {
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': config('DB_NAME'),
+           'USER': config('DB_USER'),
+           'PASSWORD': config('DB_PASSWORD'),
+           'HOST': config('DB_HOST'),
+           'PORT': '',
+       }
 
-ALLOWED_HOSTS = []
+   }
+# production
+else:
+   DATABASES = {
+       'default': dj_database_url.config(
+           default=config('DATABASE_URL')
+       )
+   }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+
 
 
 # Application definition
