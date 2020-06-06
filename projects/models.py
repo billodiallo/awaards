b91da4f3cccbdc0fa0c35613c3b5aa56cf096114
Mamadou Billo Diallo <billodiallonet@gmail.com>
2020-06-06 16:27:57 +0300

    update class profile

--- projects/models.py
+++ projects/models.py
@@ -5,7 +5,7 @@ from django.db.models import Avg
 import numpy as np
 # Create your models here.
 
-########class Profile(models.Model):
+class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
     profile_photo = models.ImageField(upload_to='profiles/',null=True)
     bio = models.CharField(max_length=240, null=True)
